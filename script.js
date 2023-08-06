065cd1d1f2700994680d0aed78ed55070eebec15
Ashmit-Verma <128896355+Ashmit-Verma@users.noreply.github.com>
2023-08-06 18:36:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+    var pannel = this.nextElementSibling;
+    if (pannel.style.display === "block") pannel.style.display = "none";
+    else pannel.style.display = "block";
+  });
+}
